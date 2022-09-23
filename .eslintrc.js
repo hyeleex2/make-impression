0afe0b7d38880423c9d4260f77aaa66f60faa441
hyeleex2 <hyeleex2@gmail.com>
2022-09-23 11:46:41 +0900

    eslint 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,13 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react/jsx-runtime',
+  ],
   overrides: [],
   parser: '@typescript-eslint/parser',
   parserOptions: {
