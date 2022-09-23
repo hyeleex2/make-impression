5d0b62cbf9b6f9cb9183c04827f6954e6a7675c7
hyeleex2 <hyeleex2@gmail.com>
2022-09-23 14:31:12 +0900

    파일 저장 영역 수정

--- components/List.tsx
+++ components/List.tsx
@@ -1,6 +1,10 @@
 import { ImageCard } from 'components/ImageCard'
 
-export const List = () => {
+export interface ListProp {
+  list: []
+}
+
+export const List = (props: ListProp) => {
   return (
     <div className="flex flex-nowrap space-x-4 w-full">
       <ImageCard />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,32 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { useRef } from 'react'
-import { List } from 'components/List'
+import { useRef, useState } from 'react'
+import { List, ListProp } from 'components/List'
 import domtoimage from 'dom-to-image'
-import { saveAs } from 'file-saver'
+// import { saveAs } from 'file-saver'
 
 const Home: NextPage = () => {
   const downloadRef = useRef<HTMLDivElement>(null)
+  const list = useState([
+    {
+      index: 0,
+    },
+  ])
 
   const clickSaveBtn = () => {
     if (downloadRef.current) {
       domtoimage.toPng(downloadRef.current).then((dataUrl: string) => {
-        // const url = window.URL.createObjectURL(blob)
-        console.log('url : ', dataUrl)
         const link = document.createElement('a')
         link.href = dataUrl
         link.download = 'impression.png'
         link.click()
-        // setTimeout(() => {
         link.remove()
         window.URL.revokeObjectURL(dataUrl)
-        // })
       })
     }
   }
+
+  const addItem = () => {}
 
   return (
     <div>
@@ -31,20 +34,24 @@ const Home: NextPage = () => {
         <title>첫인상 현인상 만들기</title>
         <meta name="description" content="첫인상 현인상 만들기" />
       </Head>
-      <div className="container md:container md:mx-auto" ref={downloadRef}>
+      <div className="container md:container md:mx-auto">
         <main className="p-4 min-w-[90%]">
-          <h3 className="font-bold text-center text-2xl m-4">첫인상/현인상 만들기</h3>
-          <div>
-            <div className="flex flex-nowrap space-x-4 w-full mb-3">
-              <div className="w-1/3 text-center">사람</div>
-              <div className="w-1/3 text-center">첫인상</div>
-              <div className="w-1/3 text-center">현인상</div>
+          <div ref={downloadRef}>
+            <h3 className="font-bold text-center text-2xl m-4">첫인상/현인상 만들기</h3>
+            <div>
+              <div className="flex flex-nowrap space-x-4 w-full mb-3">
+                <div className="w-1/3 text-center">사람</div>
+                <div className="w-1/3 text-center">첫인상</div>
+                <div className="w-1/3 text-center">현인상</div>
+              </div>
+              <List />
             </div>
-            <List />
           </div>
 
           <div className="flex flex-col mt-20 justify-center items-center">
-            <button className="block border rounded-full p-2 m-2 w-32">ADD</button>
+            <button className="block border rounded-full p-2 m-2 w-32" onClick={addItem}>
+              ADD
+            </button>
             <button className="block border rounded-full p-2 m-2 w-32" onClick={clickSaveBtn}>
               Save To Image
             </button>
