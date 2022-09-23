084989365a22ac12935a22bb45bd2451eb58c143
hyeleex2 <hyeleex2@gmail.com>
2022-09-23 15:52:15 +0900

    filesaver js로 변경;

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import { useRef, useState } from 'react'
 import { List } from 'components/List'
 import domtoimage from 'dom-to-image'
+import { saveAs } from 'file-saver'
 
 const Home: NextPage = () => {
   const downloadRef = useRef<HTMLDivElement>(null)
@@ -15,14 +16,15 @@ const Home: NextPage = () => {
   const clickSaveBtn = async () => {
     if (downloadRef.current) {
       const dataUrl = await domtoimage.toPng(downloadRef.current)
-      const link = document.createElement('a')
-      link.href = dataUrl
-      link.download = 'impression.png'
-      link.click()
-      setTimeout(() => {
-        link.remove()
-        window.URL.revokeObjectURL(dataUrl)
-      })
+      // const link = document.createElement('a')
+      // link.href = dataUrl
+      // link.download = 'impression.png'
+      // link.click()
+      saveAs(dataUrl, 'impression.png')
+      // setTimeout(() => {
+      //   link.remove()
+      //   window.URL.revokeObjectURL(dataUrl)
+      // })
     }
   }
 
@@ -34,10 +36,10 @@ const Home: NextPage = () => {
         <title>첫인상 현인상 만들기</title>
         <meta name="description" content="첫인상 현인상 만들기" />
       </Head>
-      <div className="container md:container md:mx-auto">
-        <main className="p-4 min-w-[90%]">
-          <div ref={downloadRef}>
-            <h3 className="font-bold text-center text-2xl m-4">첫인상/현인상 만들기~!~!</h3>
+      <div className="container md:container md:mx-auto min-h-screen min-w-full">
+        <main className="p-4 min-w-[90%] h-96" ref={downloadRef}>
+          <div>
+            <h3 className="font-bold text-center text-2xl m-4">첫인상/현인상 만들기</h3>
             <div>
               <div className="flex flex-nowrap space-x-4 w-full mb-3">
                 <div className="w-1/3 text-center">사람</div>
@@ -47,16 +49,15 @@ const Home: NextPage = () => {
               <List />
             </div>
           </div>
-
-          <div className="flex flex-col mt-20 justify-center items-center">
-            <button className="block border rounded-full p-2 m-2 w-32" onClick={addItem}>
-              ADD
-            </button>
-            <button className="block border rounded-full p-2 m-2 w-32" onClick={clickSaveBtn}>
-              Save To Image
-            </button>
-          </div>
         </main>
+        <div className="flex flex-col mt-20 justify-center items-center">
+          <button className="block border rounded-full p-2 m-2 w-32" onClick={addItem}>
+            ADD
+          </button>
+          <button className="block border rounded-full p-2 m-2 w-32" onClick={clickSaveBtn}>
+            Save To Image
+          </button>
+        </div>
       </div>
     </div>
   )
